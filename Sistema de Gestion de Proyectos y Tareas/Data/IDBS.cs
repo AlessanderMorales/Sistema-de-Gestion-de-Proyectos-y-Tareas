be1197e881c41bbe954c7f191749978a1eef4683
nathan95oks <nathanaelalba75@gmail.com>
2025-10-04 14:33:47 -0400

    Definir acceso data layer abstracciones e interfaces

---
+++ Sistema de Gestion de Proyectos y Tareas/Data/IDBS.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Repository
+{
+    public interface IDB<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Sistema de Gestion de Proyectos y Tareas/Data/IDbConnectionFactory.cs
@@ -0,0 +1,6 @@
+﻿using System.Data;
+
+public interface IDbConnectionSingleton
+{
+    IDbConnection CreateConnection();
+}
