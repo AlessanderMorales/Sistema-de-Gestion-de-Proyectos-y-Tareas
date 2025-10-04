d0fee3c59de916beecbe33c4c2558dab267f09f6
nathan95oks <nathanaelalba75@gmail.com>
2025-10-04 14:30:05 -0400

    Definicion de modelos y configuracion inicial

---
+++ Sistema de Gestion de Proyectos y Tareas/Models/Proyecto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Models
+{
+    public class Proyecto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+    }
+}
---
+++ Sistema de Gestion de Proyectos y Tareas/Models/Tarea.cs
@@ -0,0 +1,10 @@
+﻿namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public string? Prioridad { get; set; }
+    }
+}
