e64743b313d0c493625366ec725e613172249337
nathan95oks <nathanaelalba75@gmail.com>
2025-10-04 15:01:48 -0400

    Implementar Razor Page Usuarios

--- Sistema de Gestion de Proyectos y Tareas/Pages/Usuarios/Create.cshtml.cs
+++ Sistema de Gestion de Proyectos y Tareas/Pages/Usuarios/Create.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
+using Sistema_de_Gestion_de_Proyectos_y_Tareas.Factories;
+using Sistema_de_Gestion_de_Proyectos_y_Tareas.Models;
 
 namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Usuarios
 {
@@ -19,7 +21,33 @@ namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Usuarios
 
         public void OnGet()
         {
+            using Microsoft.AspNetCore.Mvc;
+            using Microsoft.AspNetCore.Mvc.RazorPages;
+            using Sistema_de_Gestion_de_Proyectos_y_Tareas.Factories;
+            using Sistema_de_Gestion_de_Proyectos_y_Tareas.Models;
+
+namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Usuarios
+    {
+        public class CreateModel : PageModel
+        {
+            private readonly IRepositoryFactory _factory;
+            [BindProperty]
+            public Usuario Usuario { get; set; } = new();
+
+            public CreateModel(IRepositoryFactory factory) => _factory = factory;
+
+            public void OnGet() { }
+
+            public async Task<IActionResult> OnPostAsync()
+            {
+                if (!ModelState.IsValid) return Page();
+                var repo = _factory.CreateUsuarioRepository();
+                await repo.AddAsync(Usuario);
+                return RedirectToPage("./Index");
+            }
         }
+    }
+}
 
         public IActionResult OnPost()
         {
--- Sistema de Gestion de Proyectos y Tareas/Pages/Usuarios/Index.cshtml.cs
+++ Sistema de Gestion de Proyectos y Tareas/Pages/Usuarios/Index.cshtml.cs
@@ -1,39 +1,20 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using MySql.Data.MySqlClient;
-using System.Data;
+using Sistema_de_Gestion_de_Proyectos_y_Tareas.Factories;
+using Sistema_de_Gestion_de_Proyectos_y_Tareas.Models;
 
 namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Usuarios
 {
     public class IndexModel : PageModel
     {
-        public DataTable UsuariosTable { get; set; } = new DataTable();
-        private readonly IConfiguration _configuration;
+        private readonly IRepositoryFactory _factory;
+        public IEnumerable<Usuario> Usuarios { get; private set; } = new List<Usuario>();
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IRepositoryFactory factory) => _factory = factory;
+
+        public async Task OnGetAsync()
         {
-            _configuration = configuration;
-        }
-
-        public void OnGet()
-        {
-            SelectUsuarios();
-        }
-
-        private void SelectUsuarios()
-        {
-            string connectionString = _configuration.GetConnectionString("MySqlConecction")!;
-            string query = @"SELECT id_usuario, primer_nombre, segundo_nombre, apellidos, rol 
-                             FROM Usuario
-                             WHERE estado = 1
-                             ORDER BY apellidos, primer_nombre";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                MySqlCommand command = new MySqlCommand(query, connection);
-                connection.Open();
-                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
-                adapter.Fill(UsuariosTable);
-            }
+            var repo = _factory.CreateUsuarioRepository();
+            Usuarios = await repo.GetAllAsync();
         }
     }
 }
