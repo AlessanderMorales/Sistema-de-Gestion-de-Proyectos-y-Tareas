2cccc736ad3d49d4c26a718408abc382ad48a1e4
nathan95oks <nathanaelalba75@gmail.com>
2025-10-04 15:02:38 -0400

    Implement Razor Page Tareas

--- Sistema de Gestion de Proyectos y Tareas/Pages/Tareas/Index.cshtml.cs
+++ Sistema de Gestion de Proyectos y Tareas/Pages/Tareas/Index.cshtml.cs
@@ -1,39 +1,20 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using MySql.Data.MySqlClient;
-using System.Data;
+using Sistema_de_Gestion_de_Proyectos_y_Tareas.Factories;
+using Sistema_de_Gestion_de_Proyectos_y_Tareas.Models;
 
 namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Tareas
 {
     public class IndexModel : PageModel
     {
-        public DataTable TareasTable { get; set; } = new DataTable();
-        private readonly IConfiguration _configuration;
+        private readonly IRepositoryFactory _factory;
+        public IEnumerable<Tarea> Tareas { get; private set; } = new List<Tarea>();
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IRepositoryFactory factory) => _factory = factory;
+
+        public async Task OnGetAsync()
         {
-            _configuration = configuration;
-        }
-
-        public void OnGet()
-        {
-            SelectTareas();
-        }
-
-        private void SelectTareas()
-        {
-            string connectionString = _configuration.GetConnectionString("MySqlConecction")!;
-            string query = @"SELECT id_tarea, titulo, descripcion, prioridad, fechaRegistro 
-                             FROM Tareas
-                             WHERE estado = 1
-                             ORDER BY prioridad, titulo";
-
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                MySqlCommand command = new MySqlCommand(query, connection);
-                connection.Open();
-                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
-                adapter.Fill(TareasTable);
-            }
+            var repo = _factory.CreateTareaRepository();
+            Tareas = await repo.GetAllAsync();
         }
     }
 }
