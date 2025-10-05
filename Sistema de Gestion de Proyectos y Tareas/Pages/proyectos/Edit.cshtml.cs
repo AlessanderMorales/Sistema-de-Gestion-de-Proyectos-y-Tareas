469c2e7855bfe90ed3b6eb5bad8d143a8a952956
AlessanderMorales <alessmoralesmo@gmail.com>
2025-10-05 11:48:41 -0400

    Refact

--- Sistema de Gestion de Proyectos y Tareas/Pages/proyectos/Edit.cshtml.cs
+++ Sistema de Gestion de Proyectos y Tareas/Pages/proyectos/Edit.cshtml.cs
@@ -18,7 +18,6 @@ namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Proyectos
         [BindProperty]
         public Proyecto Proyecto { get; set; } = default!;
 
-        // Maneja la solicitud GET para mostrar el formulario de edición (SÍNCRONA)
         public IActionResult OnGet(int? id)
         {
             if (id == null)
@@ -27,7 +26,7 @@ namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Proyectos
             }
 
             IDB<Proyecto> repo = _repositoryCreator.CreateRepository();
-            var proyecto = repo.GetByIdAsync(id.Value); // Llamada síncrona
+            var proyecto = repo.GetByIdAsync(id.Value);
 
             if (proyecto == null)
             {
@@ -37,8 +36,7 @@ namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Proyectos
             return Page();
         }
 
-        // OnPostAsync puede seguir siendo async si tu UpdateAsync es async o si quieres usar await para otras cosas
-        public async Task<IActionResult> OnPostAsync() // Puedes cambiarlo a 'OnPost()' si todas las operaciones del repo son síncronas
+        public async Task<IActionResult> OnPostAsync() 
         {
             if (!ModelState.IsValid)
             {
@@ -46,7 +44,7 @@ namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages.Proyectos
             }
 
             IDB<Proyecto> repo = _repositoryCreator.CreateRepository();
-            repo.UpdateAsync(Proyecto); // Llamada síncrona (si UpdateAsync en IDB es void)
+            repo.UpdateAsync(Proyecto); 
 
             return RedirectToPage("./Index");
         }
