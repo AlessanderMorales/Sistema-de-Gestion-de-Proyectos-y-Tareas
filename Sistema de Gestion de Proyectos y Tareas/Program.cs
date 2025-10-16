8bbc628f61863cde633e21f9f6254c0039f96467
JaimeTadeo <deugartetadeo@gmail.com>
2025-10-16 00:13:10 -0400

    fix:se agrego vistas de cada rol

--- Sistema de Gestion de Proyectos y Tareas/Program.cs
+++ Sistema de Gestion de Proyectos y Tareas/Program.cs
@@ -1,6 +1,4 @@
-﻿// Archivo: Program.cs (Versión Actualizada con Políticas)
-
-using Sistema_de_Gestion_de_Proyectos_y_Tareas.Application.Services;
+﻿using Sistema_de_Gestion_de_Proyectos_y_Tareas.Application.Services;
 using Sistema_de_Gestion_de_Proyectos_y_Tareas.Domain.Entities;
 using Sistema_de_Gestion_de_Proyectos_y_Tareas.Infrastructure.Persistence.Data;
 using Sistema_de_Gestion_de_Proyectos_y_Tareas.Infrastructure.Persistence.Factories;
@@ -56,11 +54,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
