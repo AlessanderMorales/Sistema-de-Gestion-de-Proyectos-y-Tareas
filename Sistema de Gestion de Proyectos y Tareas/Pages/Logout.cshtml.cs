ae36f12831dddf56bf6fcb084a9f6e36d12c182e
AlessanderMorales <alessmoralesmo@gmail.com>
2025-10-15 22:47:28 -0400

    Cerrar Session

---
+++ Sistema de Gestion de Proyectos y Tareas/Pages/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+// Archivo: Pages/Logout.cshtml.cs
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gestion_de_Proyectos_y_Tareas.Pages
+{
+    public class LogoutModel : PageModel
+    {
+
+        public IActionResult OnGet()
+        {
+            return RedirectToPage("/Index");
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToPage("/Login/Login"); 
+        }
+    }
+}
