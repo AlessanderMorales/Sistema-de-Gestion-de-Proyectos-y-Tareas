6b8cabe412c18b20a6c2f1fafc6ffa32e16b2ca7
fernando <gualterbelmonte@gmail.com>
2025-10-15 23:34:47 -0400

    Fix:Comentario arreglado

--- Sistema de Gestion de Proyectos y Tareas/Pages/Comentarios/Create.cshtml.cs
+++ Sistema de Gestion de Proyectos y Tareas/Pages/Comentarios/Create.cshtml.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Sistema_de_Gestion_de_Proyectos_y_Tareas.Application.Services;
 using Sistema_de_Gestion_de_Proyectos_y_Tareas.Domain.Entities;
